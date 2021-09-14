ed7aba08ca6ce3d2936ed81669d48275ad3bfbcc
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-09-14 15:02:22 +0545

    simple map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import { useState } from 'react';
 import './App.css';
 import Counter from './Counter';
-import Employee from './Employee';
+// import Employee from './Employee';
 
 
 function App() {
-  const data=[
-   {
-  id:'sas',
-  name: 'mane Schuster',
-  salary:1000,
-  title: ' Operations Producer',
-  address:{
-    street:'fhaufh',
-    salary:5000,
-    country:'hfuahfuf',
-    zone:{
-      map:'dadad',
-      ward:'010'
-    }
-  }
- }]
+//   const data=[
+//    {
+//   id:'sas',
+//   name: 'mane Schuster',
+//   salary:1000,
+//   title: ' Operations Producer',
+//   address:{
+//     street:'fhaufh',
+//     salary:5000,
+//     country:'hfuahfuf',
+//     zone:{
+//       map:'dadad',
+//       ward:'010'
+//     }
+//   }
+//  }]
+
+const emplyoee=[{
+  id:12443,
+  name:'nimesh',
+  title:'cs'
+},{
+  id:124433,
+  name:'nimfewfweesh',
+  title:'cs'
+},{
+  id:123567,
+  name:'dadada',
+  title:'cs'
+},{
+  id:1787623,
+  name:'suhguad',
+  title:'cs'
+}]
  
   return (
 
@@ -28,10 +46,16 @@ function App() {
     <div className="App">
       <header className="App-header">
        <Counter/>
-       {data.map(details=>{
+       {emplyoee.map((details,index)=>{
+        return(
+          <h1>{`employee ${details.name} tiitle ${details.tiitle}`}</h1>
+        )
+       })
+       }
+       {/* {data.map(details=>{
          console.log({...details})
          return(<Employee {...details} key={details.id} />)
-       })}
+       })} */}
       </header>
 
      
--- src/Employee.js
+++ src/Employee.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Employee = (props) => {
-const {name,address:{street,country,zone,salary:adressSalary},title,salary:Employesalary}=props
+// const {name,address:{street,country,zone,salary:adressSalary},title,salary:Employesalary}=props
 
 
   return (
     <div>
-      <div>this is the name:{name} title= {street} address:{adressSalary}</div>
+      {/* <div>this is the name:{name} title= {street} address:{adressSalary}</div> */}
     </div>
   )
 }
