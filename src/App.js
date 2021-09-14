022c67415e156830922d254c955b1e5d5a1215d2
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-09-14 17:12:25 +0545

    conditional renderiong

--- src/App.js
+++ src/App.js
@@ -1,62 +1,18 @@
-import { useState } from 'react';
+
 import './App.css';
-import Counter from './Counter';
-import Employee from './Employee';
-
-
 function App() {
-//   const data=[
-//    {
-//   id:'sas',
-//   name: 'mane Schuster',
-//   salary:1000,
-//   title: ' Operations Producer',
-//   address:{
-//     street:'fhaufh',
-//     salary:5000,
-//     country:'hfuahfuf',
-//     zone:{
-//       map:'dadad',
-//       ward:'010'
-//     }
-//   }
-//  }]
-
-const emplyoee=[{
-  id:12443,
-  name:'nimesh',
-  title:'cs'
-},{
-  id:124433,
-  name:'nimfewfweesh',
-  title:'cs'
-},{
-  id:123567,
-  name:'dadada',
-  title:'cs'
-},{
-  id:1787623,
-  name:'suhguad',
-  title:'cs'
-}]
- 
+  const empData=['nimesh','hvghsvuh','hhfh']
   return (
-
-  
     <div className="App">
       <header className="App-header">
-       <Counter/>
-       {emplyoee.map((details,index)=>{
-        
-        return(
-           <Employee name={details.name} key={details.id}/>)
-      
-       })
-       }
-       {/* {data.map(details=>{
-         console.log({...details})
-         return(<Employee {...details} key={details.id} />)
-       })} */}
+      {empData && empData.length>1 &&
+      <div>
+        {empData.map((details)=>{
+          return(<h4>{details}</h4>)
+        })}
+      </div>
+      }
+
       </header>
 
      
