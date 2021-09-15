21a72de899295ca5e7bab6e6810c36e83f56e7bb
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-09-15 14:57:28 +0545

    useEffect Complete and finishj

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 
 import './App.css';
-import { useState } from 'react';
-import Counter from './Counter';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+// import Counter from './Counter';
 
 function App() {
+  const [todos, settodos] = useState('')
   
+  useEffect(() => {
+    const fetchApi=async () =>{
+    const {data}= await axios.get(`https://jsonplaceholder.typicode.com/todos`)
+    // .then(response=>{
+    //   console.log(response.data)
+      settodos(data)
+    // }).catch(response=>{
+
+    // console.log('eerr')
+    // })
+    console.log(data)}
+    fetchApi()
+  }, [])
+
   return (
     <div className="App">
       
       <header className="App-header">
-      <Counter/>
+      <div>
+       {todos &&
+       todos.map((details)=>{
+         const{userId,id,title}=details
+         return(
+           <h1 key={id}>{title}</h1>
+         )
+       })
+       }
+      </div>
       </header>
 
      
--- src/Counter.js
+++ src/Counter.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from 'react'
 const Counter = () => {
   let [count, setcount] = useState(0)
